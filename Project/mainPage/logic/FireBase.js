d90667061863b8e1466f9147db4ab07b745c83de
Mahmoud Mohammed <mahmoud_spiderman@yahoo.com>
2025-12-12 00:11:29 +0200

    Update FireBase.js

--- Project/mainPage/logic/FireBase.js
+++ Project/mainPage/logic/FireBase.js
@@ -24,13 +24,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7sw2Q34Qh5-TKF3EZXY-RAtotfZu8Ec0",
-    authDomain: "maf21-de0fc.firebaseapp.com",
-    projectId: "maf21-de0fc",
-    storageBucket: "maf21-de0fc.firebasestorage.app",
-    messagingSenderId: "532823832819",
-    appId: "1:53823832819:web:b86b5d5ea808edea4c9f86",
-    measurementId: "G-N30F9SD4J8"
+    
 };
 
 const app = initializeApp(firebaseConfig);
@@ -39,5 +33,6 @@ const db = getFirestore(app);
 
 
 const user = JSON.parse(localStorage.getItem("user"));
+
 
 export { user,app,getDocs,deleteDoc, auth, db, doc,setDoc, getDoc,onSnapshot, addDoc, collection, updateDoc, query, orderBy, limit, serverTimestamp, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged };
